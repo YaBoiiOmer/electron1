04b52f21cf5ac7d1fb741fe57bceaa8f96e07b60
YaBoiiOmer <omer.h1910@gmail.com>
2022-08-13 14:17:54 +0300

    added titlebar not arranged yet

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const {app, BrowserWindow} = require('electron')
 require('electron-reload')(__dirname);
 const createWindow = () =>{
     const win = new BrowserWindow({
-        width: 800,
+        width: 400,
         height:600,
         frame: false
     });
